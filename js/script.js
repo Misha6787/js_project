9ef330df70e187677e3e33fbf232f7d36e329a8e
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-21 15:18:57 +0300

    fixed(js/script.js)fixed value

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ const   incomeAdd = document.getElementsByTagName('button')[0],
         expensesAdd = document.getElementsByTagName('button')[1],
         depositCheck = document.querySelector('#deposit-check');
 
-const   budgetMonthValue = document.querySelector(' .result-total .budget_month-value'),
-        budgetdayValue = document.querySelector(' .result-total .budget_day-value'),
-        expensesMonthValue = document.querySelector(' .result-total .expenses_month-value'),
-        additionalIncomeValue = document.querySelector(' .result-total .additional_income-value'),
-        additionalExpensesValue = document.querySelector(' .result-total .additional_expenses-value'),
-        incomePeriodValue = document.querySelector(' .result-total .income_period-value'),
-        targetMonthValue = document.querySelector(' .result-total .target_month-value');
+const   budgetMonthValue = document.querySelector('.budget_month-value'),
+        budgetdayValue = document.querySelector('.budget_day-value'),
+        expensesMonthValue = document.querySelector('.expenses_month-value'),
+        additionalIncomeValue = document.querySelector('.additional_income-value'),
+        additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+        incomePeriodValue = document.querySelector('.income_period-value'),
+        targetMonthValue = document.querySelector('.target_month-value');
 
 const   additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
         start = document.getElementById('start'),
-        salaryAmount  = document.querySelector('.salary-amount '),
+        salaryAmount  = document.querySelector('.salary-amount'),
         incomeTitle = document.querySelector('.income-items .income-title'),
         incomeAmount = document.querySelector('.income-amount'),
         expensesTitle = document.querySelector('.expenses-items .expenses-title'),
